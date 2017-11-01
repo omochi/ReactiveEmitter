7792d00cc1db93d9e53030c07b55a2316ad3b1e2
omochi <omochi.metaru@gmail.com>
2017-11-01 16:11:52 +0900

    mac

---
+++ Projects/mac/ReactiveEmitter-mac/ReactiveEmitter-mac/ReactiveEmitter_mac.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveEmitter_mac.h
+//  ReactiveEmitter-mac
+//
+//  Created by omochimetaru on 2017/11/01.
+//  Copyright © 2017年 omochimetaru. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveEmitter_mac.
+FOUNDATION_EXPORT double ReactiveEmitter_macVersionNumber;
+
+//! Project version string for ReactiveEmitter_mac.
+FOUNDATION_EXPORT const unsigned char ReactiveEmitter_macVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveEmitter_mac/PublicHeader.h>
+
+
